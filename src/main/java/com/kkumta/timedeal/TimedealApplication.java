d1092f2fbf1d234b84910711b17e318872dd5ec9
Choi Dasom <dasomchoi52@gmail.com>
2023-03-01 06:17:57 +0900

    프로젝트 생성

---
+++ src/main/java/com/kkumta/timedeal/TimedealApplication.java
@@ -0,0 +1,13 @@
+package com.kkumta.timedeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimedealApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(TimedealApplication.class, args);
+    }
+    
+}
