75ef74a6f1e1585a6b1be397b6390360dbd3dd9c
Choi Dasom <54059732+kkumta@users.noreply.github.com>
2023-03-07 04:13:03 +0900

    Revert "Closes #2 Feature/login"

--- src/main/java/com/kkumta/timedeal/domain/User.java
+++ src/main/java/com/kkumta/timedeal/domain/User.java
@@ -6,8 +6,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,31 +22,25 @@ public class User {
     private Long id;
     
     @Column(nullable = false)
-    @NotBlank
     @Size(min = 2, max = 20)
     private String name;
     
     @Column(nullable = false)
     @Size(min = 5, max = 50)
-    @Email
     private String email;
     
     @Column(nullable = false)
-    @NotBlank
     @Size(min = 8, max = 50)
     private String password;
     
     @Column(nullable = false)
-    @NotBlank
     private String userType;
     
     @Column(nullable = false)
-    @NotBlank
     @Size(max = 20)
     private String phoneNumber;
     
     @Column(nullable = false)
-    @NotBlank
     @Size(max = 100)
     private String address;
     
--- src/main/java/com/kkumta/timedeal/exception/user/InvalidCredentialsException.java
+++
@@ -1,5 +0,0 @@
-package com.kkumta.timedeal.exception.user;
-
-public class InvalidCredentialsException extends IllegalArgumentException {
-
-}
